ef4cedf279fdb102c0e79050df8db34b128cda53
shariyerShazan <shariyershazan1@gmail.com>
2025-08-06 01:17:59 +0600

    category wise product on home page

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,18 @@ function page() {
   return (
     <div className='bg-gray-100'>
       <HomePage />
-     
+      <div>
+      <h2 className='text-center text-3xl font-bold text-myColor mt-6'>Product for Men's:</h2>
+        <CategoryProduct category={"men's clothing"} />
+      </div>
+      <div>
+      <h2 className='text-center text-3xl font-bold text-myColor mt-6'>Product for Women's:</h2>
+        <CategoryProduct category={"women's clothing"} />
+      </div>
+      <div>
+      <h2 className='text-center text-3xl font-bold text-myColor mt-6'>Jewelry:</h2>
+        <CategoryProduct category={"jewelry"} />
+      </div>
       
     </div>
   )
